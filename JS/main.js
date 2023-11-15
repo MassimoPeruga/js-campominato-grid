25f0c7d8919f1a7cb2f53137cf7712215a39a0b4
MassimoPeruga <spamma-mutevole0p@icloud.com>
2023-11-15 19:34:45 +0100

    feat: bonus

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,28 @@
 'use strict';
 
+// Creazione delle celle
 function createElement(tag, classes, content) {
+    function addClasses(element, classes) {
+        for (let i = 0; i < classes.length; i++) {
+            element.classList.add(classes[i]);
+        }
+    }
     const element = document.createElement(tag);
-    element.classList.add(classes);
+    addClasses(element, classes);
     element.textContent = content;
     return element;
 }
 
+// Gestione del click su play
 document.getElementById('play').addEventListener('click', function () {
     // Pulisci la console
     console.clear();
 
     const grid = document.getElementById('ms_griglia');
     const messaggio = document.getElementById('messaggio');
+    const difficulty = document.getElementById('mode').value;
+
+    // Nascondi il messaggio visualizzato
     if (!messaggio.classList.contains('d-none')) {
         messaggio.classList.add('d-none');
     }
@@ -21,15 +31,32 @@ document.getElementById('play').addEventListener('click', function () {
         grid.removeChild(grid.firstChild);
     }
 
-    for (let i = 1; i <= 100; i++) {
-        const cellElement = createElement('div', ['ms_cella'], i);
+    // Associa il numero di righe e colonne alla difficoltà
+    let rows, cols;
+
+    if (difficulty === 'easy') {
+        rows = cols = 7;
+    } else if (difficulty === 'medium') {
+        rows = cols = 9;
+    } else if (difficulty === 'hard') {
+        rows = cols = 10;
+    } else {
+        rows = cols = 9; // Valore di default se la difficoltà non è riconosciuta
+    }
+
+    // Richiamo la funzione di creazione delle celle in base alla difficoltà
+    for (let i = 1; i <= rows * cols; i++) {
+        const cellElement = createElement('div', ['ms_cella', difficulty], i);
+        cellElement.addEventListener('click', function () {
+            cellElement.classList.add('ms_clicked');
+            console.log("Ho cliccato su:", i);
+        });
         grid.appendChild(cellElement);
     }
 
-    grid.addEventListener('click', function (event) {
-        event.target.classList.add('ms_clicked');
-        const cellNumber = Number(event.target.textContent);
-        console.log("Ho cliccato su:", cellNumber);
-    }
-    );
+    // grid.addEventListener('click', function (event) {
+    //     event.target.classList.add('ms_clicked');
+    //     const cellNumber = Number(event.target.textContent);
+    //     console.log("Ho cliccato su:", cellNumber);
+    // });
 });
--- README.md
+++ README.md
@@ -14,3 +14,18 @@ Questo programma genera una griglia di celle numerate quando l'utente preme il p
 
     6. Viene aggiunto un event listener a tutte le celle all'interno della griglia. Quando una cella viene cliccata, cambia colore e viene stampato il numero della cella cliccata nella console del browser.
 
+## Bonus
+
+Gestione della creazione dinamica di una griglia di celle in base alla difficoltà selezionata dall'utente.
+
+1. Aggiunta di una funzione che accetta due argomenti: un elemento HTML e un array di classi. Tramite un ciclo for la funzione aggiunge le classi ricevute all'elemento HTML.
+
+2. Modifica della funzione di creazione delle celle che utilizza lanuova funzione per aggiungere più classi invece di utilizzare classList.add.
+
+3. Definizione di una variabile che tiene conto il valore della difficoltà selezionata.
+
+6. In base al valore della difficoltà scelta dall'utente si assegna il numero di righe e colonne della griglia.
+
+7. Modifica del ciclo for: 
+    - in numero di celle create dipende dal prodotto delle righe per le colonne.
+    - aggiunta all'array di classi da aggiungere alle celle di una classe con il nome corrispondente alla difficoltà selezionata
